6a0bf38fdeae494d1f44a61f49ba5ce810bd1e7e
Evgenia Polozova <eugpoloz@gmail.com>
2018-08-20 21:48:26 +0300

    add configs to create a webpack module

---
+++ src/CommonTypes.js
@@ -0,0 +1,20 @@
+// @flow
+type Device = {|
+  id: string,
+  name: string,
+  power: number,
+  duration: number,
+  mode?: "night" | "day"
+|}
+
+type Rate = {|
+  from: number,
+  to: number,
+  value: number
+|}
+
+export type Input = {|
+  devices: Array<Device>,
+  rates: Array<Rate>,
+  maxPower: number
+|}
